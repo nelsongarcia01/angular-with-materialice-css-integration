78fbee8121f28fd5e9579b0d8ad6fa03a08f7dfa
Anonymous Kira <nelsonmanuelgarcia980@gmail.com>
2025-04-16 17:18:32 +0100

    feat: conclusion

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import * as $ from 'jquery';
+import * as M from 'materialize-css';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
